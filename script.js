1b6f7102639a8de3e827516ed3429de865ab36f3
aliergarang <aliergarang16@gmail.com>
2022-02-09 19:44:19 +0300

    Use user input to generate grid squares

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const gridMainContainer = document.querySelector('.grid-squares');
 
-let gridSquare = document.querySelectorAll('.grid-square');
+const input = prompt("Enter the size of grid");
 
 const gridSquares = (squaresNum) => {
+    gridMainContainer.style.setProperty('--gridSizeColumn', squaresNum);
+    gridMainContainer.style.setProperty('--gridSizeRow', squaresNum);
     for (let i = 1; i <= squaresNum; i++) {
-        gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        gridMainContainer.appendChild(gridSquare);
+        for (let i = 1; i <= squaresNum; i++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid-square');
+            gridMainContainer.appendChild(gridSquare);
+        }
+
     }
 }
 
-gridSquares(21);
+gridSquares(input);
