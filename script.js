3f0c4e8c7a9b0fe080ac0a558c12faba146365d5
aliergarang <aliergarang16@gmail.com>
2022-02-11 14:16:12 +0300

    Change prompt to input field,add instruction text,create clear&create btns

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const gridMainContainer = document.querySelector('.grid-squares');
 const instructionText = document.getElementById('instruction');
-
-const input = prompt("Enter the size of grid");
+const userInput = document.getElementById('user-input');
+const createBtn = document.getElementById('create');
+const clearBtn = document.getElementById('clear');
+let guideText = instructionText.innerText;
 
 const gridSquares = (squaresNum) => {
     gridMainContainer.style.setProperty('--gridSizeColumn', squaresNum);
     if (squaresNum > 100) {
         instructionText.innerText = 'Enter a grid square size not greater than 100';
+        instructionText.style.color = 'red';
     } else {
+        instructionText.innerText = guideText;
         for (let i = 1; i <= squaresNum; i++) {
             for (let i = 1; i <= squaresNum; i++) {
                 const gridSquare = document.createElement('div');
@@ -19,4 +23,18 @@ const gridSquares = (squaresNum) => {
     }
 }
 
-gridSquares(input);
+createBtn.addEventListener('click', e => {
+    gridSquares(+userInput.value);
+    if (userInput.value === "") {
+        instructionText.style.color = 'red';
+        instructionText.innerText = "Enter a grid square size";
+    }
+
+});
+clearBtn.addEventListener('click', e => {
+    gridMainContainer.innerHTML = "";
+    userInput.value = "";
+    instructionText.style.color = 'white';
+    instructionText.innerText = guideText;
+
+});
