dce62d5e25ddeb83eaa9f96e7b7714782ac4a92e
aliergarang <aliergarang16@gmail.com>
2022-02-11 10:49:10 +0300

    Remove row & Check user input

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const gridMainContainer = document.querySelector('.grid-squares');
+const instructionText = document.getElementById('instruction');
 
 const input = prompt("Enter the size of grid");
 
 const gridSquares = (squaresNum) => {
     gridMainContainer.style.setProperty('--gridSizeColumn', squaresNum);
-    gridMainContainer.style.setProperty('--gridSizeRow', squaresNum);
-    for (let i = 1; i <= squaresNum; i++) {
+    if (squaresNum > 100) {
+        instructionText.innerText = 'Enter a grid square size not greater than 100';
+    } else {
         for (let i = 1; i <= squaresNum; i++) {
-            const gridSquare = document.createElement('div');
-            gridSquare.classList.add('grid-square');
-            gridMainContainer.appendChild(gridSquare);
+            for (let i = 1; i <= squaresNum; i++) {
+                const gridSquare = document.createElement('div');
+                gridSquare.classList.add('grid-square');
+                gridMainContainer.appendChild(gridSquare);
+            }
         }
 
     }
